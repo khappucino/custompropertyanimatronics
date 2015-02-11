768a3f7d727631880087e1d0bafa65a40d1c731e
khappucino <david.capino@gmail.com>
2015-02-10 19:53:27 -0800

    Initial Commit

---
+++ layertest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  layertest
+//
+//  Created by spacehomunculus on 2/10/15.
+//  Copyright (c) 2015 electricbaconstudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ layertest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  layertest
+//
+//  Created by spacehomunculus on 2/10/15.
+//  Copyright (c) 2015 electricbaconstudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
